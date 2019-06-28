cb4ebce055931f0b021ef94b1534173a44e12f30
Gino Messmer <messmer@medialesson.de>
2019-06-28 09:26:15 +0200

    Create JWT token on Discord sign-in

--- src/main/src/xyz/dommi/servlets/LoginSuccessServlet.java
+++ src/main/src/xyz/dommi/servlets/LoginSuccessServlet.java
@@ -2,13 +2,17 @@ package xyz.dommi.servlets;
 
 import bell.oauth.discord.domain.User;
 import bell.oauth.discord.main.Response;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.mongodb.DB;
 import xyz.dommi.beans.UserBean;
+import xyz.dommi.common.EnvironmentConfig;
 import xyz.dommi.db.DBConnection;
 import xyz.dommi.db.UserDB;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,17 +29,30 @@ public class LoginSuccessServlet extends HttpServlet {
         Response res = LoginServlet.builder.exchange(code);
 
         if (res == Response.ERROR) {
-           System.out.println("Error");
-        } else {
-            User user =  LoginServlet.builder.getUser();
-
-            DBConnection dbConnection = new DBConnection();
-            DB db = dbConnection.connect();
-            UserDB userDB = new UserDB(db);
-            userDB.createUser(user.getId(),user.getUsername(),user.getEmail());
-            UserBean userBean = new UserBean(user.getId());
-            request.setAttribute("user", userBean);
-            request.getServletContext().getRequestDispatcher("/").forward(request,response);
+            System.out.println("Error");
+            return;
         }
+
+        // Otherwise create user
+        User user = LoginServlet.builder.getUser();
+
+        // Create JWT token and pass it to response cookies
+        Algorithm algorithm = Algorithm.HMAC256(EnvironmentConfig.getSecretKey());
+        String jwt = JWT.create()
+                .withIssuer("discord")
+                .withAudience("redhead")
+                .withSubject(user.getId())
+                .sign(algorithm);
+
+        response.addCookie(new Cookie("token", jwt));
+
+        // Establish DB connection and add user
+        DBConnection dbConnection = new DBConnection();
+        DB db = dbConnection.connect();
+        UserDB userDB = new UserDB(db);
+        userDB.createUser(user.getId(), user.getUsername(), user.getEmail());
+        UserBean userBean = new UserBean(user.getId());
+        request.setAttribute("user", userBean);
+        request.getServletContext().getRequestDispatcher("/").forward(request, response);
     }
 }
