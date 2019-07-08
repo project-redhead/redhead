0845da06a30ce83563c47957fae05c5e9f6cd878
Kevin Bobbe <k.bobbe00@gmail.com>
2019-07-08 11:05:56 +0200

    Add Transactions

--- src/main/src/xyz/dommi/db/UserDB.java
+++ src/main/src/xyz/dommi/db/UserDB.java
@@ -1,8 +1,10 @@
 package xyz.dommi.db;
 
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBObject;
+import org.json.JSONArray;
 import xyz.dommi.requests.Response;
 import xyz.dommi.requests.ResponseType;
 
@@ -54,6 +56,32 @@ public class UserDB extends DBManager {
         setIntByID(id, "points", points);
     }
 
+    public void addPoints(String id, int points, String description){
+        setPoints(id, getPoints(id)+points);
+        addTransaction(id, points, description);
+    }
+
+    public void addPoints(String id, int points){
+        setPoints(id, getPoints(id)+points);
+    }
+
+    public boolean remPoints(String id, int points, String description){
+        if(getPoints(id) < points){
+            return false;
+        }
+        setPoints(id, getPoints(id)-points);
+        addTransaction(id, -points, description);
+        return true;
+    }
+
+    public boolean remPoints(String id, int points){
+        if(getPoints(id) < points){
+            return false;
+        }
+        setPoints(id, getPoints(id)-points);
+        return true;
+    }
+
     public Response addBet(String id, String gameid, int amount, int option) {
         BetGameDB gameDB = new BetGameDB(db);
         if (gameDB.isBetTimeValid(gameid)) {
@@ -61,7 +89,7 @@ public class UserDB extends DBManager {
                 if (option >= 0 && option < gameDB.getOptions(gameid).length) {
                     if (amount > 0) {
                         if (getPoints(id) >= amount) {
-                            setPoints(id, getPoints(id) - amount);
+                            remPoints(id, amount, "Wette eingereicht.");
 
                             return new Response(ResponseType.OK, new BasicDBObject("_id", gameDB.addBet(gameid, id, amount, option)));
                         }
@@ -82,6 +110,21 @@ public class UserDB extends DBManager {
         return true;
     }
 
+    public void addTransaction(String id, int value, String description){
+
+        int _id = getTransactions(id).length();
+
+        BasicDBObject transaction = new BasicDBObject("_id", _id)
+                .append("description", description)
+                .append("value", value);
+
+        pushDBObjectByID(id, "transactions", transaction);
+    }
+
+    public JSONArray getTransactions(String id) {
+        return getJSONArrayByID(id, "transactions");
+    }
+
     public boolean userExists(String id) {
         return getObjectByID(id) != null;
     }
@@ -92,7 +135,8 @@ public class UserDB extends DBManager {
                     .append("name", name)
                     .append("email", email)
                     .append("points", 0)
-                    .append("roleid", "user");
+                    .append("roleid", "user")
+                    .append("transactions", new BasicDBList());
             getCollection().insert(user);
         }
 
