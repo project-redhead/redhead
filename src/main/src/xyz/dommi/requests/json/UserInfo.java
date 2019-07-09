73f78978f53a4c4bdf8af4e04005f63381f9876f
d0mmi <dommihd@gmail.com>
2019-07-09 10:25:40 +0200

    Now returns Error if no User is found

--- src/main/src/xyz/dommi/requests/json/UserInfo.java
+++ src/main/src/xyz/dommi/requests/json/UserInfo.java
@@ -1,6 +1,7 @@
 package xyz.dommi.requests.json;
 
 import com.mongodb.DB;
+import com.mongodb.DBObject;
 import xyz.dommi.common.HttpUtils;
 import xyz.dommi.db.DBConnection;
 import xyz.dommi.db.UserDB;
@@ -25,7 +26,13 @@ public class UserInfo extends JsonRequest {
             DB db = DBConnection.getInstance().connect();
             UserDB userDB = new UserDB(db);
 
-            return new Response(ResponseType.OK, userDB.getUser(id));
+            DBObject user = userDB.getUser(id);
+
+            if(user == null){
+                return new Response(ResponseType.ERROR,"User not found");
+            }
+
+            return new Response(ResponseType.OK, user);
         }
         return new Response(ResponseType.ERROR,"Id not valid");
     }
